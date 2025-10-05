b34c0a3f725413fb8fbe39b79f317ef38e076995
Matthew <caspr117@gmail.com>
2025-10-05 19:38:09 -0400

    Insert Many

--- table.py
+++ table.py
@@ -5,8 +5,15 @@ conn = sqlite3.connect("first.db")
 c = conn.cursor()
 
 
+many_customers = [
+                    ("matt", "jay", "value@fff.com"),
+                    ("stephaine" , " john", "stuff@333.com"),
+                    ("jack", "slater", "viction@ddd.com")
+                 ]
 
-c.execute("INSERT INTO customers VALUES ('John', 'Elder', 'john@GMEM.com')")
+
+
+c.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers )
 #commit command
 conn.commit()
 
